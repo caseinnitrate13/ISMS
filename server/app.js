80f413143fffade26ae3a238b9e8e66e3dab2f7d
Neca B. Dulay <113820687+YukiItazura@users.noreply.github.com>
2025-03-27 17:29:57 +0800

    1st commit
    node modules
    document-submission

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const port = 8080;
+
+
+const app = express();
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const template = fs.readFileSync(path.join(__dirname, '..', 'public', 'template.html'), 'utf-8');
+
+
+app.get('/progress-tracker', (req, res) => {
+    const dashboard = fs.readFileSync(path.join(__dirname, '..', 'public', 'progress-tracker.html'), 'utf-8');
+    res.send(template.replace('{{content}}', dashboard));
+});
+
+
+app.get('/submission', (req, res) => {
+    const dashboard = fs.readFileSync(path.join(__dirname, '..', 'public', 'document-submission.html'), 'utf-8');
+    res.send(template.replace('{{content}}', dashboard));
+});
+
+
+
+
+
+
+
+app.use((req, res) => {
+    res.status(404).send(template.replace('{{content}}', '<h3>404 - Page Not Found</h3>'));
+});
+
+app.listen(port, () =>{
+    console.log(`Server successful, listening on port http://127.0.0.1:${port}`)
+})
